319c5c6d670bb1357c4a8de831fc4fb5a191c100
emmap88 <emmap8@hotmail.com>
2023-07-28 06:32:38 -0400

    Create findKey.js

---
+++ findKey.js
@@ -0,0 +1,29 @@
+const findKey = (obj, callback) => {
+  for (const key in obj) {
+    if (callback(obj[key])) {
+      return key;
+    }
+  }
+};
+
+const assertEqual = function(actual, expected) {
+
+  if (actual === expected) {
+    console.log("✅✅✅ Assertion Passed: " + (actual + " === " + expected));
+  }
+  else if (actual !== expected) {
+    console.log("🛑🛑🛑 Assertion Failed: " + (actual + " !== " + expected));
+  }
+}
+
+const result = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri": { stars: 3 },
+  "noma": { stars: 2 },
+  "elBulli": { stars: 3 },
+  "Ora": { stars: 2 },
+  "Akelarre": { stars: 3 }
+}, x => x.stars === 2);
+
+assertEqual(result, "noma");
+
