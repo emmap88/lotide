ecaa45f6fea6874ad4a18ce07fcdb2a23806c8f1
emmap88 <emmap8@hotmail.com>
2023-07-28 03:48:44 -0400

    Create assertObjectsEqual.js

---
+++ assertObjectsEqual.js
@@ -0,0 +1,28 @@
+const assertEqual = require('./assertEqual');
+const eqArrays = require('./eqArrays');
+const eqObjects = require('./eqObjects');
+
+const assertObjectsEqual = function(actual, expected) {
+  const inspect = require('util').inspect;
+
+  if (actual, expected) {
+    console.log("✅✅✅ Assertion Passed: ", actual, "===", inspect(expected));
+  } else {
+    console.log("🛑🛑🛑 Assertion Failed: ", actual, "!==", inspect(expected));
+  }
+}
+
+const ab = {
+  a: "1",
+  b: "2"
+};
+
+const ba = {
+  b: "2",
+  a: "1"
+}
+
+assertObjectsEqual(ab, ba);
+
+
+
