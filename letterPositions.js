b336cbe2601a84996e6d3583f58c3d1c63211e4a
emmap88 <emmap8@hotmail.com>
2023-07-28 02:08:22 -0400

    Create letterPositions.js

---
+++ letterPositions.js
@@ -0,0 +1,15 @@
+const assertArraysEqual = require("./assertArraysEqual");
+const eqArrays = require("./eqArrays");
+
+const letterPositions = function(sentence) {
+  const result = {};
+  for (let i = 0; i < sentence.length; i++) {
+    if (result[sentence[i]]) {
+      result[sentence[i]].push(i);
+    } else {
+      result[sentence[i]] = [i];
+    }
+  }
+  return result;
+};
+console.log(letterPositions("lighthouse in the house"));
