d59c4d653e2654b6434d34576ba55c9ea442a56e
emmap88 <emmap8@hotmail.com>
2023-07-28 00:55:42 -0400

    Create without.js

---
+++ without.js
@@ -0,0 +1,24 @@
+const assertEqual = require("./assertEqual");
+const eqArrays = require("./eqArrays");
+const assertArraysEqual = require("./assertArraysEqual");
+
+const without = function(arr1, arr2){
+  let arr = [];
+
+  if(arr1, arr2)
+  return arr;
+
+  for(let i = 0; i < arr1.length; i++){
+    if(arr1[i] !== arr2[0]){
+      arr.push(arr1[i]);
+    }
+  }
+  return arr;
+}
+
+//console.log(without([1, 2, 3], [1])) // => [2, 3]
+//console.log(without(["1", "2", "3"], [1, 2, "3"])) // => ["1", "2"]
+const words = ["hello", "world", "lighthouse"];
+//console.log(without(words, ["lighthouse"])); // no need to capture return value for this test case
+// Make sure the original array was not altered by the without function
+console.log(words, ["hello", "world", "lighthouse"]);
