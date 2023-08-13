042127561ba6f49832aed3340f43aa0e67c9697f
emmap88 <emmap8@hotmail.com>
2023-08-13 02:50:59 -0400

    Update tail.js

--- tail.js
+++ tail.js
@@ -15,3 +15,5 @@ assertEqual(tail(["Yo Yo", "Lighthouse", " Labs"]), "Labs");
 const words = ["Yo Yo", " Lighthouse", "Labs"];
 tail(words); // no need to capture the return value since we are not checking it
 assertEqual(words.length, 3); // original array should still have 3 elements!
+
+module.exports = tail;
