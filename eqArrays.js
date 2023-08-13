0ce55d1d2902bc75268ba4eaf9e1fc06290f2b11
emmap88 <emmap8@hotmail.com>
2023-08-13 01:28:35 -0400

    Update eqArrays.js

--- eqArrays.js
+++ eqArrays.js
@@ -1,21 +1,21 @@
-const assertEqual = require("./assertEqual");
+const assertEqual = require('./assertEqual');
 
-const eqArrays = function(arr1, arr2){
-  if(arr1.length !== arr2.length){
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
     return false;
   }
-  for(let i = 0; i < arr1.length; i++){
-    if (arr1[i] !== arr2[i]){
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
       return false;
     }
   }
   return true;
-}
-console.log(eqArrays([1, 2, 3], [1, 2, 3])) // => true
-console.log(eqArrays([1, 2, 3], [3, 2, 1])) // => false
+};
+console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true
+console.log(eqArrays([1, 2, 3], [3, 2, 1])); // => false
 
-console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])) // => true
-console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])) // => false
+console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // => true
+console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])); // => false
 console.log(eqArrays(([1, 2, 3], [1, 2, 3]), true));
 
 
