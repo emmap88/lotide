45c6ad63f2b4045e249a47c762a6229ab9ab8bbb
emmap88 <emmap8@hotmail.com>
2023-09-02 22:11:45 -0400

    Update countOnly.js

--- countOnly.js
+++ countOnly.js
@@ -2,7 +2,7 @@ const countOnly = function(allItems, itemsToCount) {
   const results = {};
 
   for (const item of allItems) {
-    if (itemsToCount[item]) { 
+    if (itemsToCount[item]) {
       if (results[item]) {
         results[item] += 1;
       } else {
@@ -13,7 +13,7 @@ const countOnly = function(allItems, itemsToCount) {
   }
 
   return results;
-}
+};
 const firstNames = [
   "Karl",
   "Salima",
