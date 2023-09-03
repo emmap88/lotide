d3504cb14681ee21b97a28b090a95f598c807db5
emmap88 <emmap8@hotmail.com>
2023-09-02 23:36:36 -0400

    Create tailTest.js

---
+++ test/tailTest.js
@@ -0,0 +1,15 @@
+const assert = require('chai').assert;
+const tail = require('../tail');
+
+describe("#tail", () => {
+  const words = ["Cat Cat", "Dog Dog", "Lighthouse", "Labs"];
+  it("returns 3 for words.length", () => {
+    assert.strictEqual((words.length), 3);
+  });
+  it("returns 2 for tail(words).length", () => {
+    assert.strictEqual(tail(words).length, 2);
+  });
+  it("returns ['Lighthouse', 'Labs'] for tail(words)", () => {
+    assert.deepEqual(tail(words), ['Lighthouse', 'Labs']);
+  });
+});
