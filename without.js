3719df19bbab0594d4ecc77382c2d160b4d0d227
emmap88 <emmap8@hotmail.com>
2023-09-02 23:09:41 -0400

    Update without.js

--- without.js
+++ without.js
@@ -1,9 +1,7 @@
 const without = function(arr1, arr2) {
   let arr = [];
-
   if (arr1, arr2)
     return arr;
-
   for (let i = 0; i < arr1.length; i++) {
     if (arr1[i] !== arr2[0]) {
       arr.push(arr1[i]);
@@ -11,11 +9,4 @@ const without = function(arr1, arr2) {
   }
   return arr;
 };
-
 module.exports = without;
-//console.log(without([1, 2, 3], [1])) // => [2, 3]
-//console.log(without(["1", "2", "3"], [1, 2, "3"])) // => ["1", "2"]
-// const words = ["hello", "world", "lighthouse"];
-// //console.log(without(words, ["lighthouse"])); // no need to capture return value for this test case
-// // Make sure the original array was not altered by the without function
-// console.log(words, ["hello", "world", "lighthouse"]);
