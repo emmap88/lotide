3089f4bf769d1a7e0acc792274e587cc1b5948ed
emmap88 <emmap8@hotmail.com>
2023-09-02 23:09:13 -0400

    Update findKey.js

--- findKey.js
+++ findKey.js
@@ -1,5 +1,3 @@
-//const assertEqual = require('./assertEqual');
-
 const findKey = (obj, callback) => {
   for (const key in obj) {
     if (callback(obj[key])) {
@@ -7,16 +5,4 @@ const findKey = (obj, callback) => {
     }
   }
 };
-
 module.exports = findKey;
-
-// const result = findKey({
-//   "Blue Hill": { stars: 1 },
-//   "Akaleri": { stars: 3 },
-//   "noma": { stars: 2 },
-//   "elBulli": { stars: 3 },
-//   "Ora": { stars: 2 },
-//   "Akelarre": { stars: 3 }
-// }, x => x.stars === 2);
-
-// assertEqual(result, "noma");
