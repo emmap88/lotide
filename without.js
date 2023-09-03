fd14b0618b235d64d2da51c808fd2c11125f2bef
emmap88 <emmap8@hotmail.com>
2023-09-02 22:16:23 -0400

    Update without.js

--- without.js
+++ without.js
@@ -1,7 +1,3 @@
-const assertEqual = require("./assertEqual");
-const eqArrays = require("./eqArrays");
-const assertArraysEqual = require("./assertArraysEqual");
-
 const without = function(arr1, arr2) {
   let arr = [];
 
@@ -16,9 +12,10 @@ const without = function(arr1, arr2) {
   return arr;
 };
 
+module.exports = without;
 //console.log(without([1, 2, 3], [1])) // => [2, 3]
 //console.log(without(["1", "2", "3"], [1, 2, "3"])) // => ["1", "2"]
-const words = ["hello", "world", "lighthouse"];
-//console.log(without(words, ["lighthouse"])); // no need to capture return value for this test case
-// Make sure the original array was not altered by the without function
-console.log(words, ["hello", "world", "lighthouse"]);
+// const words = ["hello", "world", "lighthouse"];
+// //console.log(without(words, ["lighthouse"])); // no need to capture return value for this test case
+// // Make sure the original array was not altered by the without function
+// console.log(words, ["hello", "world", "lighthouse"]);
