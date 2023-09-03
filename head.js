a875d87a0b9e7199fb17fdb9adff41757da5a653
emmap88 <emmap8@hotmail.com>
2023-09-02 22:53:35 -0400

    Update head.js

--- head.js
+++ head.js
@@ -6,7 +6,6 @@
 const head = (arr) => {
   return arr[0];
 };
-
+module.exports = head;
 // assertEqual(head([5, 6, 7]), 5);
 // assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-module.exports = head;
