0f1205a975136c0d2e0ccada3383f2f0964f4cd4
emmap88 <emmap8@hotmail.com>
2023-09-02 22:20:15 -0400

    Update map.js

--- map.js
+++ map.js
@@ -1,8 +1,3 @@
-const assertArraysEqual = require("./assertArraysEqual");
-const eqArrays = require("./eqArrays");
-
-const words = ["ground", "control", "to", "major", "tom"];
-
 const map = function(array, callback) {
   const results = [];
   for (let item of array) {
