92728622477c9a2faf5c97a808782c8139711784
emmap88 <emmap8@hotmail.com>
2023-09-02 23:09:24 -0400

    Update head.js

--- head.js
+++ head.js
@@ -1,11 +1,4 @@
-// const assertEqual = require('./assertEqual');
-
-// assertEqual("Lighthouse Labs", "Bootcamp");
-// assertEqual(1, 2, 3);
-
 const head = (arr) => {
   return arr[0];
 };
 module.exports = head;
-// assertEqual(head([5, 6, 7]), 5);
-// assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
