01c854b1a6a41cffc8529f48d29fc184f1d62d3c
emmap88 <emmap8@hotmail.com>
2023-09-02 23:09:34 -0400

    Update middle.js

--- middle.js
+++ middle.js
@@ -1,23 +1,8 @@
-// const assertEqual = require("./assertEqual");
-// const eqArrays = require("./eqArrays");
-// const assertArraysEqual = require("./assertArraysEqual");
-
 const middle = function(arr) {
   if (arr.length < 3) return [];
-
   const mid = Math.floor(arr.length / 2);
-
   return arr.length % 2 ?
     arr.slice(mid, mid + 1) : // Odd
     arr.slice(mid - 1, mid + 1); // Even
-
 };
-
 module.exports = middle;
-
-// console.log(middle([1]));
-// console.log(middle([1, 2]));
-// console.log(middle([1, 2, 3]));
-// console.log(middle([1, 2, 3, 4, 5]));
-// console.log(middle([1, 2, 3, 4]));
-// console.log(middle([1, 2, 3, 4, 5, 6]));
