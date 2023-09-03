e9ac3d8b9e6dd0ea7ed78d7282dea5a486c0b9a5
emmap88 <emmap8@hotmail.com>
2023-09-02 23:09:37 -0400

    Update tail.js

--- tail.js
+++ tail.js
@@ -1,18 +1,6 @@
-// const assertEqual = require('./assertEqual');
-
-// assertEqual("Lighthouse Labs", "Bootcamp");
-// assertEqual(1, 1);
-
 const tail = (newArr) => {
   {
     return newArr.slice(1);
-  //return newArr.splice(1)
   }
 };
 module.exports = tail;
-/* Test Case: Check the original array*/
-// assertEqual(tail(["Yo Yo", "Lighthouse", " Labs"]), "Labs");
-// const words = ["Yo Yo", " Lighthouse", "Labs"];
-// tail(words); // no need to capture the return value since we are not checking it
-// assertEqual(words.length, 3); // original array should still have 3 elements!
-
