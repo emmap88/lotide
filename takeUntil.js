09876fc4a1798264d8d7104801855fdfaf7f72ce
emmap88 <emmap8@hotmail.com>
2023-09-06 19:58:42 -0400

    Update takeUntil.js

--- takeUntil.js
+++ takeUntil.js
@@ -1,12 +1,18 @@
-const takeUntil = function(array, callback) {
+const takeUntil = function (array, callback) {
+  // Initialize a new array to store the results
   let newArray = [];
+  // Loop through the input array
   for (const i in array) {
+    // If callback returns true, stop the loop
     if (callback(array[i])) {
       break;
-    } else {
+    } // Otherwise add the element to the results array
+    else {
       newArray.push(array[i]);
     }
   }
+  // Return the results array
   return newArray;
 };
+// Export the function
 module.exports = takeUntil;
