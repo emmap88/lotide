22150127118c0eac89848aa271a76f47930f916d
emmap88 <emmap8@hotmail.com>
2023-09-06 19:58:22 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,24 @@
 // index.js
-const head = require('./head');
-const tail = require('./tail');
-const middle = require('./middle');
-const assertArraysEqual = require('./assertArraysEqual');
-const assertEqual = require('./assertEqual');
-const assertObjectsEqual = require('./assertObjectsEqual');
-const countLetters = require('./countLetters');
-const countOnly = require('./countOnly');
-const eqArrays = require('./eqArrays');
-const eqObjects = require('./eqObjects');
-const findKey = require('./findKey');
-const findKeyByValue = require('./findKeyByValue');
-const flatten = require('./flatten');
-const letterPositions = require('./letterPositions');
-const map = require('./map');
-const takeUntil = require('./takeUntil');
-const without = require('./without');
+const head = require("./head"); // gets head function from head.js
+const tail = require("./tail"); // gets tail function from tail.js
+const middle = require("./middle"); // gets middle function from middle.js
+const assertArraysEqual = require("./assertArraysEqual"); // gets assertArraysEqual function from assertArraysEqual.js
+const assertEqual = require("./assertEqual"); // gets assertEqual function from assertEqual.js
+const assertObjectsEqual = require("./assertObjectsEqual"); // gets assertObjectsEqual function from assertObjectsEqual.js
+const countLetters = require("./countLetters"); // gets countLetters function from countLetters.js
+const countOnly = require("./countOnly"); // gets countOnly function from countOnly.js
+const eqArrays = require("./eqArrays"); // gets eqArrays function from eqArrays.js
+const eqObjects = require("./eqObjects"); // gets eqObjects function from eqObjects.js
+const findKey = require("./findKey"); // get findKey function from findKey.js
+const findKeyByValue = require("./findKeyByValue"); // get findKeyByValue function from findKeyByValue.js
+const flatten = require("./flatten"); // get flatten function from flatten.js
+const letterPositions = require("./letterPositions"); // get letterPositions function from letterPositions.js
+const map = require("./map"); // get map function from map.js
+const takeUntil = require("./takeUntil"); // get takeUntil function from takeUntil.js
+const without = require("./without"); // get without function from without.js
 
 module.exports = {
+  // exports all functions
   head,
   tail,
   middle,
@@ -34,5 +35,5 @@ module.exports = {
   letterPositions,
   map,
   takeUntil,
-  without
+  without,
 };
