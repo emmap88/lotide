37c463060f5dd8452506eae90a7015b3ae6be719
emmap88 <emmap8@hotmail.com>
2023-09-06 19:58:47 -0400

    Update without.js

--- without.js
+++ without.js
@@ -1,12 +1,21 @@
-const without = function(arr1, arr2) {
-  let arr = [];
-  if (arr1, arr2)
-    return arr;
+const without = function (arr1, arr2) {
+  // Validate input
+  // If either array is falsy, return empty array
+  if (!arr1 || !arr2) {
+    return [];
+  }
+  // Initialize empty result array
+  let result = [];
+  // Loop through arr1
   for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[0]) {
-      arr.push(arr1[i]);
+    // Check if current element is NOT in arr2
+    if (!arr2.includes(arr1[i])) {
+      // If not found, push element to result array
+      result.push(arr1[i]);
     }
   }
-  return arr;
+  // Return result array without elements from arr2
+  return result;
 };
+// Export function
 module.exports = without;
