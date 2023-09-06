c352b2794a79b83b31875df8cbd6eec6742924ae
emmap88 <emmap8@hotmail.com>
2023-09-06 19:58:30 -0400

    Update middle.js

--- middle.js
+++ middle.js
@@ -1,8 +1,14 @@
-const middle = function(arr) {
+const middle = function (arr) {
+  // If the array has less than 3 elements, return an empty array
   if (arr.length < 3) return [];
+  // Calculate the middle index of the array by floor dividing the length by 2
   const mid = Math.floor(arr.length / 2);
-  return arr.length % 2 ?
-    arr.slice(mid, mid + 1) : // Odd
-    arr.slice(mid - 1, mid + 1); // Even
+  // Check if array length is odd or even
+  return arr.length % 2
+    ? // If odd, return the middle element
+      arr.slice(mid, mid + 1)
+    : // If even, return the two middle elements
+      arr.slice(mid - 1, mid + 1);
 };
+// Export the function
 module.exports = middle;
