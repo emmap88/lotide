4f794e356a13ffa7b6734b5bb364b31b54f97f13
emmap88 <emmap8@hotmail.com>
2023-09-06 19:57:47 -0400

    Update assertEqual.js

--- assertEqual.js
+++ assertEqual.js
@@ -1,8 +1,12 @@
-const assertEqual = function(actual, expected) {
+const assertEqual = function (actual, expected) {
+  // Compare actual and expected values
   if (actual === expected) {
+    // If equal, print success message
     console.log("✅✅✅ Assertion Passed: " + (actual + " === " + expected));
   } else if (actual !== expected) {
+    // If not equal, print failure message
     console.log("🛑🛑🛑 Assertion Failed: " + (actual + " !== " + expected));
   }
 };
+// Export the function
 module.exports = assertEqual;
