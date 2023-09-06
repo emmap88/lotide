ee65efd7e79d3896ff09e5aa8238dc61e45b865a
emmap88 <emmap8@hotmail.com>
2023-09-06 19:58:12 -0400

    Update findKeyByValue.js

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,8 +1,12 @@
-const findKeyByValue = function(object, value) {
+const findKeyByValue = function (object, value) {
+  // Loop through each key in the object
   for (const key in object) {
+    // If the value at the current key matches the target value
     if (object[key] === value) {
+      // Return the matching key
       return key;
     }
   }
 };
+// Export the function
 module.exports = findKeyByValue;
