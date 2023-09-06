a1b58488f1a5deb49b23cd5a116b7c2423ed21c9
emmap88 <emmap8@hotmail.com>
2023-09-06 19:58:27 -0400

    Update map.js

--- map.js
+++ map.js
@@ -1,8 +1,14 @@
-const map = function(array, callback) {
+const map = function (array, callback) {
+  // Initialize an empty array to store the results
   const results = [];
+  // Loop through each item in the array
   for (let item of array) {
+    // Call the callback function, passing in the item
+    // And push the result of the callback to the results array
     results.push(callback(item));
   }
+  // Return the results array
   return results;
 };
+// Export the function
 module.exports = map;
