fcee064bc951f569135551d6597aecb4b5a25b16
emmap88 <emmap8@hotmail.com>
2023-09-06 19:57:42 -0400

    Update assertArraysEqual.js

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,10 +1,15 @@
-const eqArrays = require("./eqArrays");
+const eqArrays = require("./eqArrays"); // import eqArrays function
 
-const assertArraysEqual = function(arr1, arr2) {
-  if (arr1, arr2) {
+const assertArraysEqual = function (arr1, arr2) {
+  // assertArraysEqual compares two arrays
+  if (eqArrays(arr1, arr2)) {
+    // if eqArrays returns true (arrays are equal)
     console.log("✅✅✅ Assertion Passed: ", arr1, "===", arr2);
+    // print success message
   } else {
+    // otherwise, print failure message
     console.log("🛑🛑🛑 Assertion Failed: ", arr1, "!==", arr2);
   }
 };
-module.exports = assertArraysEqual;
+// Export the function
+module.exports = assertArraysEqual; 
