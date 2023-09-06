b3d51ec87a197c86b3a5e80f2c62bd424b695fc3
emmap88 <emmap8@hotmail.com>
2023-09-06 19:57:50 -0400

    Update assertObjectsEqual.js

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,11 +1,17 @@
-const eqObjects = require('./eqObjects');
-
-const assertObjectsEqual = function(actual, expected) {
-  const inspect = require('util').inspect;
-  if (actual, expected) {
+// Import eqObjects function to compare objects
+const eqObjects = require("./eqObjects");
+// assertObjectsEqual function compares two objects
+const assertObjectsEqual = function (actual, expected) {
+  // Import inspect from util to print objects
+  const inspect = require("util").inspect;
+  // If objects are equal
+  if (eqObjects(actual, expected)) {
+    // Print success message
     console.log("✅✅✅ Assertion Passed: ", actual, "===", inspect(expected));
   } else {
+    // Print failure message
     console.log("🛑🛑🛑 Assertion Failed: ", actual, "!==", inspect(expected));
   }
 };
+// Export the function
 module.exports = assertObjectsEqual;
