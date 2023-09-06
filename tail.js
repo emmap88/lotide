3cf3c13d64f043fa8d823c3fb9d3632f3a9e02d2
emmap88 <emmap8@hotmail.com>
2023-09-06 19:58:40 -0400

    Update tail.js

--- tail.js
+++ tail.js
@@ -1,6 +1,10 @@
 const tail = (newArr) => {
+  // Use .slice() to return a new array
+  // containing all elements except the first
+  // Slice from index 1 to the end
   {
     return newArr.slice(1);
   }
 };
+// Export the function
 module.exports = tail;
