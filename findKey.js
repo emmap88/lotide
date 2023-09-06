7dfd06d3804ed7d558a66053103136c3be2fa7bd
emmap88 <emmap8@hotmail.com>
2023-09-06 19:58:10 -0400

    Update findKey.js

--- findKey.js
+++ findKey.js
@@ -1,8 +1,12 @@
 const findKey = (obj, callback) => {
+  // Loop through each key in the object
   for (const key in obj) {
+    // Call the callback function, passing in the value associated with the current key
     if (callback(obj[key])) {
+      // If the callback returns true, return the current key
       return key;
     }
   }
 };
+// Export the function
 module.exports = findKey;
