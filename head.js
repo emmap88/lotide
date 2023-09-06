2a4e0f06db85bd7f3e0f25f05ff2ff4b23aea03a
emmap88 <emmap8@hotmail.com>
2023-09-06 19:58:17 -0400

    Update head.js

--- head.js
+++ head.js
@@ -1,4 +1,7 @@
+// Defines a head function that takes an array as a parameter
+// and returns the first element of the array
 const head = (arr) => {
   return arr[0];
 };
+// Exports the function
 module.exports = head;
